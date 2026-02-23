3a603a7b4ae5461359b7667e989baf4debb3ab36
kaustuvprajapati <kaustuvprajapati@gmail.com>
2026-02-23 10:04:24 +0000

    fix: Disable zsh-autosuggestions, use kwik-cmd instead

--- shell/zsh_autocomplete.sh
+++ shell/zsh_autocomplete.sh
@@ -1,97 +1,86 @@
 #!/bin/zsh
 # kwik-cmd Zsh Auto-suggestions
-# Shows suggestions as you type, like zsh-autosuggestions
+# Replaces zsh-autosuggestions with kwik-cmd powered suggestions
 
 # Check if kwik-cmd exists
 (( $+commands[kwik-cmd] )) || return
 
-# Suggestion state
-typeset -g KWIK_SUGGESTION=""
-typeset -g KWIK_LAST_BUFFER=""
+# Disable zsh-autosuggestions if running
+if (( $+functions[_zsh_autosuggest_disable] )); then
+    _zsh_autosuggest_disable
+fi
 
-# Get suggestion for current input
-kwik-fetch-suggestion() {
+# State
+typeset -g KWIK_SUGGESTION=""
+typeset -g KWIK_BUFFER=""
+
+# Fetch suggestion
+kwik-fetch() {
     local buffer="$BUFFER"
-    local cursor=$CURSOR
     
-    # Don't suggest for empty or very short input
+    # Only fetch if buffer changed
+    [ "$buffer" = "$KWIK_BUFFER" ] && return
+    KWIK_BUFFER="$buffer"
+    
+    # Don't suggest for empty or very short
     (( ${#buffer} < 1 )) && return
     
     # Skip ignored commands
     local base="${buffer%% *}"
     case "$base" in
-        cd|ls|ll|la|l|ls-|pwd|echo|exit|export|declare|typeset|unset|shift|\
+        cd|ls|ll|la|l|pwd|echo|exit|export|declare|typeset|unset|shift|\
         local|readonly|help|which|what|time|fg|bg|jobs|kill|builtin|test|\
         true|false|logout|shopt|umask|set|setenv|printenv|eval|exec|source|alias|unalias|\
-        sudo|rm|mkdir|touch|cat|grep|sed|awk|find|xargs|sort|uniq|head|tail|wc)
+        sudo|rm|mkdir|touch|cat|grep|sed|awk|find|xargs|sort|uniq|head|tail|wc|clear)
+            KWIK_SUGGESTION=""
             return
             ;;
     esac
     
     # Get suggestion
     KWIK_SUGGESTION=$(kwik-cmd suggest "$buffer" 2>/dev/null | sed -n '2p' | sed 's/^[ ]*[0-9]*\. //')
-    KWIK_LAST_BUFFER="$buffer"
-}
-
-# Widget: Accept suggestion
-kwik-accept-suggestion() {
-    if [ -n "$KWIK_SUGGESTION" ] && [ "$BUFFER" != "$KWIK_SUGGESTION" ]; then
-        BUFFER="$KWIK_SUGGESTION"
-        CURSOR=${#BUFFER}
-        KWIK_SUGGESTION=""
-    fi
-}
-
-# Widget: Accept and execute
-kwik-accept-and-run() {
-    if [ -n "$KWIK_SUGGESTION" ]; then
-        BUFFER="$KWIK_SUGGESTION"
-        zle accept-line
-    fi
-}
-
-# Widget: Clear suggestion
-kwik-clear-suggestion() {
-    KWIK_SUGGESTION=""
-}
-
-# Widget: Show suggestion (bind to cursor movement)
-kwik-suggest-show() {
-    # Only fetch if buffer changed
-    if [ "$BUFFER" != "$KWIK_LAST_BUFFER" ]; then
-        kwik-fetch-suggestion
-    fi
     
-    # Show in zle message area
+    # Display suggestion
     if [ -n "$KWIK_SUGGESTION" ]; then
-        zle -M "==> $KWIK_SUGGESTION"
+        zle -M "$KWIK_SUGGESTION"
     else
         zle -M ""
     fi
 }
 
+# Accept suggestion
+kwik-accept() {
+    if [ -n "$KWIK_SUGGESTION" ] && [ "$BUFFER" != "$KWIK_SUGGESTION" ]; then
+        BUFFER="$KWIK_SUGGESTION"
+        CURSOR=${#BUFFER}
+        KWIK_SUGGESTION=""
+        zle -M ""
+    fi
+}
+
+# Clear
+kwik-clear() {
+    KWIK_SUGGESTION=""
+    KWIK_BUFFER=""
+}
+
 # Create widgets
-zle -N kwik-accept-suggestion
-zle -N kwik-accept-and-run
-zle -N kwik-clear-suggestion
-zle -N kwik-suggest-show
+zle -N kwik-fetch
+zle -N kwik-accept
+zle -N kwik-clear
 
 # Bind keys
-bindkey "^F" kwik-accept-suggestion         # Ctrl+F to accept
-bindkey "^E" kwik-accept-suggestion         # Ctrl+E to accept  
-bindkey "^[[A" kwik-accept-suggestion      # Up arrow to accept
-bindkey "^[f" kwik-accept-suggestion       # Alt+f to accept word
+bindkey "^F" kwik-accept
+bindkey "^E" kwik-accept
 
-# Hook into zle - show suggestion after cursor moves
-zle -N zle-keymap-select kwik-suggest-show
-zle -N zle-line-init kwik-suggest-show
+# Hook into zle
+zle -N zle-line-init kwik-fetch
+zle -N zle-keymap-select kwik-fetch
+zle -N zle-line-finish kwik-clear
 
-# Clear on line finish
-zle -N zle-line-finish kwik-clear-suggestion
+# Initial fetch
+kwik-fetch
 
-# Initial load
-kwik-fetch-suggestion
-
-echo "kwik-cmd auto-suggestions loaded!"
-echo "Type a command and wait - suggestions appear below"
-echo "Press Ctrl+F or Up Arrow to accept suggestion"
+echo "kwik-cmd suggestions loaded!"
+echo "Type command and wait - suggestions appear below"
+echo "Press Ctrl+F or Enter to accept"
