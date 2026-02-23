36a44c0864ecfc6a7f366aa6bcff56aeee5ea270
kaustuvprajapati <kaustuvprajapati@gmail.com>
2026-02-23 10:07:47 +0000

    feat: RPROMPT-based suggestions - shows on right side of prompt

--- shell/zsh_autocomplete.sh
+++ shell/zsh_autocomplete.sh
@@ -1,28 +1,14 @@
 #!/bin/zsh
-# kwik-cmd Zsh Auto-suggestions
-# Replaces zsh-autosuggestions with kwik-cmd powered suggestions
+# kwik-cmd - Show suggestions in RPROMPT (right side of prompt)
 
 # Check if kwik-cmd exists
 (( $+commands[kwik-cmd] )) || return
 
-# Disable zsh-autosuggestions if running
-if (( $+functions[_zsh_autosuggest_disable] )); then
-    _zsh_autosuggest_disable
-fi
-
-# State
-typeset -g KWIK_SUGGESTION=""
-typeset -g KWIK_BUFFER=""
-
-# Fetch suggestion
-kwik-fetch() {
+# Update RPROMPT with suggestion
+kwik-update-rprompt() {
     local buffer="$BUFFER"
     
-    # Only fetch if buffer changed
-    [ "$buffer" = "$KWIK_BUFFER" ] && return
-    KWIK_BUFFER="$buffer"
-    
-    # Don't suggest for empty or very short
+    # Don't for empty or very short
     (( ${#buffer} < 1 )) && return
     
     # Skip ignored commands
@@ -32,55 +18,46 @@ kwik-fetch() {
         local|readonly|help|which|what|time|fg|bg|jobs|kill|builtin|test|\
         true|false|logout|shopt|umask|set|setenv|printenv|eval|exec|source|alias|unalias|\
         sudo|rm|mkdir|touch|cat|grep|sed|awk|find|xargs|sort|uniq|head|tail|wc|clear)
-            KWIK_SUGGESTION=""
+            RPROMPT=""
             return
             ;;
     esac
     
     # Get suggestion
-    KWIK_SUGGESTION=$(kwik-cmd suggest "$buffer" 2>/dev/null | sed -n '2p' | sed 's/^[ ]*[0-9]*\. //')
+    local suggestion
+    suggestion=$(kwik-cmd suggest "$buffer" 2>/dev/null | sed -n '2p' | sed 's/^[ ]*[0-9]*\. //')
     
-    # Display suggestion
-    if [ -n "$KWIK_SUGGESTION" ]; then
-        zle -M "$KWIK_SUGGESTION"
+    if [ -n "$suggestion" ]; then
+        RPROMPT="%F{243}$suggestion%f"
     else
-        zle -M ""
+        RPROMPT=""
     fi
 }
 
-# Accept suggestion
-kwik-accept() {
-    if [ -n "$KWIK_SUGGESTION" ] && [ "$BUFFER" != "$KWIK_SUGGESTION" ]; then
-        BUFFER="$KWIK_SUGGESTION"
-        CURSOR=${#BUFFER}
-        KWIK_SUGGESTION=""
-        zle -M ""
+# Accept suggestion widget
+kwik-accept-rprompt() {
+    if [ -n "$RPROMPT" ]; then
+        local suggestion
+        suggestion=$(kwik-cmd suggest "$BUFFER" 2>/dev/null | sed -n '2p' | sed 's/^[ ]*[0-9]*\. //')
+        if [ -n "$suggestion" ]; then
+            BUFFER="$suggestion"
+            CURSOR=${#BUFFER}
+            RPROMPT=""
+        fi
     fi
-}
-
-# Clear
-kwik-clear() {
-    KWIK_SUGGESTION=""
-    KWIK_BUFFER=""
 }
 
 # Create widgets
-zle -N kwik-fetch
-zle -N kwik-accept
-zle -N kwik-clear
+zle -N kwik-update-rprompt
+zle -N kwik-accept-rprompt
 
-# Bind keys
-bindkey "^F" kwik-accept
-bindkey "^E" kwik-accept
+# Hook into zle - update on every keypress
+zle -N zle-keymap-select kwik-update-rprompt
+zle -N zle-line-init kwik-update-rprompt
 
-# Hook into zle
-zle -N zle-line-init kwik-fetch
-zle -N zle-keymap-select kwik-fetch
-zle -N zle-line-finish kwik-clear
+# Accept with Ctrl+F
+bindkey "^F" kwik-accept-rprompt
 
-# Initial fetch
-kwik-fetch
-
-echo "kwik-cmd suggestions loaded!"
-echo "Type command and wait - suggestions appear below"
-echo "Press Ctrl+F or Enter to accept"
+echo "kwik-cmd RPROMPT suggestions loaded!"
+echo "Suggestions appear on the right side of your prompt"
+echo "Press Ctrl+F to accept"
